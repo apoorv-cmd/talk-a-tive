d0ad1b21616d174190a9fd173e2e31d5743b9af0
Your Name <you@example.com>
2022-01-21 23:29:59 +0530

    filecommit

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -15,7 +15,7 @@ import ScrollableChat from "./ScrollableChat";
 // import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://apoorvchatapp.herokuapp.com/"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 // var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
